b5d68c229b03fae0b4836409e2151efd977673c3
RakshithaThanasekaran <57462084+RakshithaThanasekaran@users.noreply.github.com>
2025-07-23 18:29:28 -0400

    Update settings.py

--- Downloads/ArtSuppliesLending-main/mysite/settings.py
+++ Downloads/ArtSuppliesLending-main/mysite/settings.py
@@ -32,7 +32,7 @@ environ.Env.read_env(os.path.join(BASE_DIR / '.env'))
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--#6=-nbzpzjumwl0-+ku%+xh+(c6x558@l7wemgpy%tnysl(*t'
+SECRET_KEY = 'test'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
